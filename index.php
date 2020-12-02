e41d6b9aecac7dc98403e51d4650c02e65388496
Jens <jens@jensz12.com>
2020-12-02 13:11:21 +0100

    Setup

--- index.php
+++ index.php
@@ -2,19 +2,11 @@
 
 require 'vendor/autoload.php';
 
-if (DEV)
-	require 'config/dev.mysql.php';
-else
-	require 'config/mysql.php';
-
-require 'inc/functions.php';
-
 $klein = new \Klein\Klein();
 
-//$klein->respond(function($request, //$response, $service) {
-//	$service->cities = get_cities();
-//	$service->layout('views/main.php');
-//});
+$klein->respond(function($request, $response, $service) {
+	$service->layout('views/main.php');
+});
 
 $klein->respond('GET', '/', function($request, $response, $service) {
 	$service->title = 'Ask Jens';
