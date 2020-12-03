4bf424e1d53bc6018777c0a93bfa7624815e94ec
Jens <jens@jensz12.com>
2020-12-03 17:40:37 +0100

    DBSetup

--- index.php
+++ index.php
@@ -13,6 +13,26 @@ $klein->respond('GET', '/', function($request, $response, $service) {
 	$service->render('views/front.php');
 });
 
+
+$klein->respond('GET', '/dbtest', function($request, $response, $service) {
+
+  $mysqli = new mysqli('localhost' , 'themidnight_aj' , ')@o1[Lvab%.7' , 'themidnight_aj');
+  $mysqli->set_charset('utf8');
+
+  if ($mysqli->connect_errno)
+    die('DB Connection failed, try again later');
+
+  $sql = 'SELECT * FROM articles ORDER BY date ASC';
+  $result = $mysqli->query($sql);
+ 
+  $articles = []; 
+
+
+  $service->title = 'DB Test';
+  $service->articles = $articles; 
+	$service->render('views/dbtest.php');
+});
+
 $klein->onHttpError(function ($code, $router) {
 	if ($code == 404) {
 		$service = $router->service();
--- views/front.php
+++ views/front.php
@@ -9,21 +9,5 @@
       <small class="text-muted">November 12 2017</small>
     </div>
     <p class="mb-1">“Mono no aware” is a Japanese term relating to the wistful recognition of impermanence — a melancholic awareness that things are ephemeral and we can never relive the past. Are The Midnight an embodiment of this or are they the cure?</p>
-    <small>Aaron Vehling</small>
-  </a>
-  <a href="https://newretrowave.com/2019/04/05/the-midnight-talk-tour-and-reveal-new-single-america-online/" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">The Midnight Talk Tour and Reveal New Single – America Online</h5>
-      <small class="text-muted">April 5 2019</small>
-    </div>
-    <p class="mb-1">Tim McEwan and Tyler Lyle, the Synthpop wonder duo behind The Midnight, made their 5th U.S. tour stop (Their 40th show ever) this past Wednesday in Columbus, Ohio. They played to a nearly at-capacity venue. Very impressive when you realize it was a weekday.</p>
-    <small class="text-muted">New Retro Wave</small>
-  </a>
-  <a href="http://www.thenocturnaltimes.com/the-wait-is-over-the-midnight-unleashes-monsters-album-alongside-european-tour/" class="list-group-item list-group-item-action">
-    <div class="d-flex w-100 justify-content-between">
-      <h5 class="mb-1">The Wait is Over: The Midnight Unleashes ‘MONSTERS’ Album Alongside European Tour</h5>
-      <small class="text-muted">July 10 2020</small>
-    </div>
-    <p class="mb-1">New Music Friday is bursting at the seams at the hand of one duo in particular. Today, electronic connoisseurs The Midnight reveal their brand-new album MONSTERS, alongside an incredible European Tour. </p>
-    <small class="text-muted">The Noturnal Times</small>
+    <small>Vehlinggo</small>
   </a>
