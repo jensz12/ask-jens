ee28fe77783612716b2ea49b982f13d7ad8cc7ad
Jens Møller <jensz123@gmail.com>
2021-03-05 21:51:44 +0100

    Updates
    Cookiebanner, Bootstrap 4.6.0 and GA

--- views/main.php
+++ views/main.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+	<script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="0e71c815-765d-48da-9a57-f1b2f0230a8a" data-blockingmode="auto" type="text/javascript"></script>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -15,12 +16,22 @@
 	<meta name="twitter:title" content="<?php echo $this->title; ?>">
 	<meta name="twitter:description" content="Ask Jens">
 	<meta name="twitter:image:src" content="/data/img/askjens.jpg">
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-	<link rel="image_src" href="/data/img/askjens.jpg">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">	<link rel="image_src" href="/data/img/askjens.jpg">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/5de3f22030.js" crossorigin="anonymous"></script>
 	<link rel="manifest" href="/manifest.json">
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZWFSHLP6SQ"></script>
+	<script>
+			window.dataLayer = window.dataLayer || [];
+		function gtag() { dataLayer.push(arguments); }
+		gtag('js', new Date());
+
+		gtag('config', 'G-ZWFSHLP6SQ');
+	</script>
+
+
+	<script id="CookieDeclaration" src="https://consent.cookiebot.com/0e71c815-765d-48da-9a57-f1b2f0230a8a/cd.js" type="text/javascript" async></script>
 	<style>
 		body {
 			background-color: #C0C0C0;
@@ -78,8 +89,7 @@
 		</div>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
 
 </html>
