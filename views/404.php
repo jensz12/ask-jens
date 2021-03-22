98743428f45c84d33d5ef00bf48bd7a334b1c5b0
Jens Møller <jensz123@gmail.com>
2021-03-22 03:12:07 +0100

    updated 404 page

--- views/404.php
+++ views/404.php
@@ -1,2 +1,4 @@
-<h1 class="text-center">Page not found</h1>
-<p class="text-center"><a href="/">Go to the front page</a></>
+<div class="jumbotron">
+<h1 class="text-center display-4">The page you are looking for, is in another bedroom</h1>
+<p class="text-center"><a href="/">Go to the front page</a></p>
+</div>
