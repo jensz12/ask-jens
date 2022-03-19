b9f1df4feb2c828f7f6feb368782fdc4e5f65e33
Jens <jensz123@gmail.com>
2022-03-19 14:51:24 +0100

    Bootstrap 5

--- views/article.php
+++ views/article.php
@@ -2,7 +2,9 @@
 <div class="text-center">
 	<a href="/"><img src="/data/img/askjens.png" class="img-fluid center-block" alt="Ask Jens" width="400px"></a>
 </div>
-	<div class="jumbotron">
+	<div class="card">
+	<div class="card-body">
 	<h1 class="display-4"><?php echo $this->article['headline']; ?></h1>
 	<p><?php echo $parsedown->text($this->article['content']); ?></p>
+	</div>
 	</div>
--- views/main.php
+++ views/main.php
@@ -14,8 +14,7 @@
 	<meta name="twitter:title" content="<?php echo $this->title; ?>">
 	<meta name="twitter:description" content="<?php echo $this->desc; ?>">
 	<meta name="twitter:image:src" content="/data/img/askjens.jpg">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">	<link rel="image_src" href="/data/img/askjens.jpg">
-	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href='https://fonts.googleapis.com/css?family=Roboto:100,300' rel='stylesheet' type='text/css'>
 	<script src="https://kit.fontawesome.com/5de3f22030.js" crossorigin="anonymous"></script>
 	<link rel="manifest" href="/manifest.json">
@@ -53,6 +52,5 @@
 		</div>
 	</div>
 	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-</body>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script></body>
 </html>
